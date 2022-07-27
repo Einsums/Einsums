96036cd9b9450622649757dbbd79c67a1ee76eae
Justin Turney <justin.turney@gmail.com>
2022-07-27 16:00:45 +0000

    Adds arange functions to mimic numpy.

--- src/include/einsums/Utilities.hpp
+++ src/include/einsums/Utilities.hpp
@@ -122,4 +122,28 @@ auto create_identity_tensor(const std::string &name, MultiIndex... index) -> Ten
 
     return A;
 }
+
+template <typename T>
+auto arange(T start, T stop, T step = T{1}) -> Tensor<T, 1> {
+    assert(stop >= start);
+
+    // Determine the number of elements that will be produced
+    int nelem = static_cast<int>((stop - start) / step);
+
+    auto result = create_tensor<T>("arange created tensor", nelem);
+    zero(result);
+
+    int index{0};
+    for (T value = start; value < stop; value += step) {
+        result(index++) = value;
+    }
+
+    return result;
+}
+
+template <typename T>
+auto arange(T stop) -> Tensor<T, 1> {
+    return arange(T{0}, stop);
+}
+
 } // namespace einsums
