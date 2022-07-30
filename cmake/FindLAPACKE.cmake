88e7a2884aac75214650646e772d800af21f8250
Justin Turney <justin.turney@gmail.com>
2022-07-30 22:36:00 +0000

    Incorporates a local copy of cblas and lapacke from netlib.
    For systems that do not provide cblas or lapacke.

--- cmake/FindLAPACKE.cmake
+++ cmake/FindLAPACKE.cmake
@@ -1,6 +1,10 @@
 include(FindPackageHandleStandardArgs)
 
-if (NOT LAPACKE_FOUND AND NOT HAVE_MKL_LAPACKE_HEADER)
+check_include_file(mkl_lapacke.h HAVE_MKL_LAPACKE_H)
+if (HAVE_MKL_LAPACKE_H)
+    set(LAPACKE_FOUND TRUE)
+else()
+# if (NOT LAPACKE_FOUND AND NOT HAVE_MKL_LAPACKE_H)
 
     find_path(LAPACKE_INCLUDE_DIRS
         NAMES lapacke.h
@@ -18,12 +22,14 @@ if (NOT LAPACKE_FOUND AND NOT HAVE_MKL_LAPACKE_HEADER)
     find_package_handle_standard_args(LAPACKE DEFAULT_MSG LAPACKE_LIBRARIES LAPACKE_INCLUDE_DIRS)
 
     if (LAPACKE_FOUND)
+        set(HAVE_LAPACKE_H TRUE)
+
         message(STATUS "Found components for LAPACKE.")
 
         add_library(lapacke INTERFACE)
         target_include_directories(lapacke
             INTERFACE
-                ${LAPACKE_INCLUDE_DIRS}    
+                ${LAPACKE_INCLUDE_DIRS}
         )
         target_link_libraries(lapacke
             INTERFACE
