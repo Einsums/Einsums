973edef3401f6116c50fac430e215db47d6689dd
Justin Turney <justin.turney@gmail.com>
2022-07-31 17:14:11 +0000

    Try again

--- cmake/FindCBLAS.cmake
+++ cmake/FindCBLAS.cmake
@@ -6,39 +6,42 @@ check_include_file(mkl_cblas.h HAVE_MKL_CBLAS_H)
 if (HAVE_MKL_CBLAS_H)
     set(CBLAS_FOUND TRUE)
 else()
-    find_path(CBLAS_INCLUDE_DIRS
-        NAMES cblas.h
-        HINTS ${CBLAS_DIR}
-        PATHS /usr /usr/local
-        PATH_SUFFIXES include
-    )
-
-    find_library(CBLAS_LIBRARIES
-        NAMES cblas blas
-        HINTS ${CBLAS_DIR}
-        PATHS /usr /usr/local
-        PATH_SUFFIXES lib64 lib
-    )
-
-    find_package_handle_standard_args(CBLAS DEFAULT_MSG CBLAS_LIBRARIES CBLAS_INCLUDE_DIRS)
-
-    if (CBLAS_FOUND)
-        set(HAVE_CBLAS_H TRUE)
-
-        message(STATUS "Found components for CBLAS.")
-
-        add_library(cblas INTERFACE)
-        target_include_directories(cblas
-            INTERFACE
-                ${CBLAS_INCLUDE_DIRS}
+    check_include_file(cblas.h HAVE_CBLAS_H)
+    if (HAVE_CBLAS_H)
+        find_path(CBLAS_INCLUDE_DIRS
+            NAMES cblas.h
+            HINTS ${CBLAS_DIR}
+            PATHS /usr /usr/local
+            PATH_SUFFIXES include
         )
-        target_link_libraries(cblas
-            INTERFACE
-                ${CBLAS_LIBRARIES}
+
+        find_library(CBLAS_LIBRARIES
+            NAMES cblas blas
+            HINTS ${CBLAS_DIR}
+            PATHS /usr /usr/local
+            PATH_SUFFIXES lib64 lib
         )
-        target_compile_definitions(cblas
-            INTERFACE
-                HAVE_CBLAS
-        )
+
+        find_package_handle_standard_args(CBLAS DEFAULT_MSG CBLAS_LIBRARIES CBLAS_INCLUDE_DIRS)
+
+        if (CBLAS_FOUND)
+            set(HAVE_CBLAS_H TRUE)
+
+            message(STATUS "Found components for CBLAS.")
+
+            add_library(cblas INTERFACE)
+            target_include_directories(cblas
+                INTERFACE
+                    ${CBLAS_INCLUDE_DIRS}
+            )
+            target_link_libraries(cblas
+                INTERFACE
+                    ${CBLAS_LIBRARIES}
+            )
+            target_compile_definitions(cblas
+                INTERFACE
+                    HAVE_CBLAS
+            )
+        endif()
     endif()
 endif()
--- cmake/FindLAPACKE.cmake
+++ cmake/FindLAPACKE.cmake
@@ -4,41 +4,41 @@ check_include_file(mkl_lapacke.h HAVE_MKL_LAPACKE_H)
 if (HAVE_MKL_LAPACKE_H)
     set(LAPACKE_FOUND TRUE)
 else()
-# if (NOT LAPACKE_FOUND AND NOT HAVE_MKL_LAPACKE_H)
-
-    find_path(LAPACKE_INCLUDE_DIRS
-        NAMES lapacke.h
-        HINTS ${LAPACKE_DIR}
-        PATHS /usr /usr/local
-        PATH_SUFFIXES include
-    )
-
-    find_library(LAPACKE_LIBRARIES lapacke
-        HINTS ${LAPACKE_DIR}
-        PATHS /usr /usr/local
-        PATH_SUFFIXES lib64 lib
-    )
-
-    find_package_handle_standard_args(LAPACKE DEFAULT_MSG LAPACKE_LIBRARIES LAPACKE_INCLUDE_DIRS)
-
-    if (LAPACKE_FOUND)
-        set(HAVE_LAPACKE_H TRUE)
-
-        message(STATUS "Found components for LAPACKE.")
-
-        add_library(lapacke INTERFACE)
-        target_include_directories(lapacke
-            INTERFACE
-                ${LAPACKE_INCLUDE_DIRS}
+    check_include_file(lapacke.h HAVE_LAPACKE_H)
+    if (HAVE_LAPACKE_H)
+        find_path(LAPACKE_INCLUDE_DIRS
+            NAMES lapacke.h
+            HINTS ${LAPACKE_DIR}
+            PATHS /usr /usr/local
+            PATH_SUFFIXES include
         )
-        target_link_libraries(lapacke
-            INTERFACE
-                ${LAPACKE_LIBRARIES}
+
+        find_library(LAPACKE_LIBRARIES lapacke
+            HINTS ${LAPACKE_DIR}
+            PATHS /usr /usr/local
+            PATH_SUFFIXES lib64 lib
         )
-        target_compile_definitions(lapacke
-            INTERFACE
-                HAVE_LAPACKE
-        )
+
+        find_package_handle_standard_args(LAPACKE DEFAULT_MSG LAPACKE_LIBRARIES LAPACKE_INCLUDE_DIRS)
+
+        if (LAPACKE_FOUND)
+            set(HAVE_LAPACKE_H TRUE)
+
+            message(STATUS "Found components for LAPACKE.")
+
+            add_library(lapacke INTERFACE)
+            target_include_directories(lapacke
+                INTERFACE
+                    ${LAPACKE_INCLUDE_DIRS}
+            )
+            target_link_libraries(lapacke
+                INTERFACE
+                    ${LAPACKE_LIBRARIES}
+            )
+            target_compile_definitions(lapacke
+                INTERFACE
+                    HAVE_LAPACKE
+            )
+        endif()
     endif()
-
 endif()
