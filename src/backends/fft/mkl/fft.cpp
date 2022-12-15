ef3b3076af45844525a3042a6d048af86cb7edde
Justin Turney <justin.turney@gmail.com>
2022-12-15 08:33:14 +0000

    Forward FFT working.

--- src/backends/fft/mkl/fft.cpp
+++ src/backends/fft/mkl/fft.cpp
@@ -1,22 +1,50 @@
 #include "fft.hpp"
 
+#include "einsums/Print.hpp"
+
 #include <mkl_dfti.h>
 
 namespace einsums::backend::mkl {
+
+namespace {
+inline void verify(MKL_LONG status) {
+    if (status == DFTI_NO_ERROR)
+        return;
+
+    println_abort("MKL DFTI failure: {}", status);
+}
+} // namespace
 
 void scfft(const Tensor<float, 1> &a, Tensor<std::complex<float>, 1> *result) {
     DFTI_DESCRIPTOR_HANDLE handle = nullptr;
     MKL_LONG status;
 
-    status = DftiCreateDescriptor(&handle, DFTI_SINGLE, DFTI_REAL, 1, a.dim(0));
-    status = DftiSetValue(handle, DFTI_PLACEMENT, DFTI_NOT_INPLACE);
-    status = DftiSetValue(handle, DFTI_CONJUGATE_EVEN_STORAGE, DFTI_COMPLEX_COMPLEX);
-    status = DftiCommitDescriptor(handle);
+    verify(DftiCreateDescriptor(&handle, DFTI_SINGLE, DFTI_REAL, 1, a.dim(0)));
+    verify(DftiSetValue(handle, DFTI_PLACEMENT, DFTI_NOT_INPLACE));
+    verify(DftiSetValue(handle, DFTI_CONJUGATE_EVEN_STORAGE, DFTI_COMPLEX_COMPLEX));
+    verify(DftiCommitDescriptor(handle));
 
     auto *x_real = const_cast<float *>(a.data());
     auto *x_cmplx = (MKL_Complex8 *)result->data();
 
-    status = DftiComputeForward(handle, x_real, x_cmplx);
+    verify(DftiComputeForward(handle, x_real, x_cmplx));
+
+    DftiFreeDescriptor(&handle);
+}
+
+void ccfft(const Tensor<std::complex<float>, 1> &a, Tensor<std::complex<float>, 1> *result) {
+    DFTI_DESCRIPTOR_HANDLE handle = nullptr;
+    MKL_LONG status;
+
+    verify(DftiCreateDescriptor(&handle, DFTI_SINGLE, DFTI_COMPLEX, 1, a.dim(0)));
+    verify(DftiSetValue(handle, DFTI_PLACEMENT, DFTI_NOT_INPLACE));
+    verify(DftiSetValue(handle, DFTI_CONJUGATE_EVEN_STORAGE, DFTI_COMPLEX_COMPLEX));
+    verify(DftiCommitDescriptor(handle));
+
+    auto *x_input = reinterpret_cast<MKL_Complex8 *>(const_cast<std::complex<float> *>(a.data()));
+    auto *x_output = reinterpret_cast<MKL_Complex8 *>(result->data());
+
+    verify(DftiComputeForward(handle, x_input, x_output));
 
     DftiFreeDescriptor(&handle);
 }
@@ -25,16 +53,34 @@ void dzfft(const Tensor<double, 1> &a, Tensor<std::complex<double>, 1> *result) 
     DFTI_DESCRIPTOR_HANDLE handle = nullptr;
     MKL_LONG status;
 
-    status = DftiCreateDescriptor(&handle, DFTI_DOUBLE, DFTI_REAL, 1, a.dim(0));
-    status = DftiSetValue(handle, DFTI_PLACEMENT, DFTI_NOT_INPLACE);
-    status = DftiSetValue(handle, DFTI_CONJUGATE_EVEN_STORAGE, DFTI_COMPLEX_COMPLEX);
-    status = DftiCommitDescriptor(handle);
+    verify(DftiCreateDescriptor(&handle, DFTI_DOUBLE, DFTI_REAL, 1, a.dim(0)));
+    verify(DftiSetValue(handle, DFTI_PLACEMENT, DFTI_NOT_INPLACE));
+    verify(DftiSetValue(handle, DFTI_CONJUGATE_EVEN_STORAGE, DFTI_COMPLEX_COMPLEX));
+    verify(DftiCommitDescriptor(handle));
 
     auto *x_real = const_cast<double *>(a.data());
     auto *x_cmplx = (MKL_Complex16 *)result->data();
 
-    status = DftiComputeForward(handle, x_real, x_cmplx);
+    verify(DftiComputeForward(handle, x_real, x_cmplx));
 
     DftiFreeDescriptor(&handle);
 }
+
+void zzfft(const Tensor<std::complex<double>, 1> &a, Tensor<std::complex<double>, 1> *result) {
+    DFTI_DESCRIPTOR_HANDLE handle = nullptr;
+    MKL_LONG status;
+
+    verify(DftiCreateDescriptor(&handle, DFTI_DOUBLE, DFTI_COMPLEX, 1, a.dim(0)));
+    verify(DftiSetValue(handle, DFTI_PLACEMENT, DFTI_NOT_INPLACE));
+    verify(DftiSetValue(handle, DFTI_CONJUGATE_EVEN_STORAGE, DFTI_COMPLEX_COMPLEX));
+    verify(DftiCommitDescriptor(handle));
+
+    auto *x_input = reinterpret_cast<MKL_Complex16 *>(const_cast<std::complex<double> *>(a.data()));
+    auto *x_output = reinterpret_cast<MKL_Complex16 *>(result->data());
+
+    verify(DftiComputeForward(handle, x_input, x_output));
+
+    DftiFreeDescriptor(&handle);
+}
+
 } // namespace einsums::backend::mkl
--- tests/FFT.cpp
+++ tests/FFT.cpp
@@ -1,5 +1,7 @@
 #include "einsums/FFT.hpp"
 
+#include "einsums/STL.hpp"
+
 #include <catch2/catch.hpp>
 
 // Some elements of these tests come from Intel MKL
@@ -22,26 +24,55 @@ void init_data(einsums::Tensor<T, 1> &data, int N, int H) {
     }
 }
 
+template <typename T>
+void init_data(einsums::Tensor<std::complex<T>, 1> &data, int N, int H) {
+    double TWOPI = 6.2831853071795864769, phase;
+    int n;
+
+    for (n = 0; n < N; n++) {
+        phase = moda<T>(n, H, N) / N;
+        data(n).real(cos(TWOPI * phase) / N);
+        data(n).imag(sin(TWOPI * phase) / N);
+    }
+}
+
 template <typename Source, typename Result, int N = 6, int H = -1>
 void fft1d_1() {
     using namespace einsums;
 
     auto x_data = create_tensor<Source>("sample data", N);
-    auto x_result = create_tensor<Result>("FFT result", N / 2 + 1);
+    auto x_result = create_tensor<Result>("FFT result", einsums::is_complex_v<Source> ? N : N / 2 + 1);
 
     init_data(x_data, N, H);
 
-    println(x_data);
+    // println(x_data);
 
     fft::fft(x_data, &x_result);
 
-    // Perform checks on the result.
-    println(x_result);
+    // The result of each forward transformation is a tensor with
+    // exactly one 1.0 and everything else is 0.0.
+    for (int n = 0; n < x_result.dim(0); n++) {
+        einsums::remove_complex_t<Result> re_exp = 0.0, im_exp = 0.0;
+
+        // The position of the expected 1.0 is dependent on the Source data type.
+        if constexpr (!einsums::is_complex_v<Source>) {
+            if ((n - H) % N == 0 || (-n - H) % N == 0)
+                re_exp = 1.0;
+        } else {
+            if (n == x_result.dim(0) - 1)
+                re_exp = 1.0;
+        }
+
+        REQUIRE_THAT(x_result(n).real(), Catch::Matchers::WithinAbsMatcher(einsums::remove_complex_t<Result>(re_exp), 0.0001));
+        REQUIRE_THAT(x_result(n).imag(), Catch::Matchers::WithinAbsMatcher(einsums::remove_complex_t<Result>(im_exp), 0.0001));
+    }
 }
 
 TEST_CASE("fft1") {
     fft1d_1<float, std::complex<float>>();
     fft1d_1<double, std::complex<double>>();
-
     fft1d_1<double, std::complex<double>, 10>();
+
+    fft1d_1<std::complex<float>, std::complex<float>>();
+    fft1d_1<std::complex<double>, std::complex<double>>();
 }
