93ff10fa3197595466248673dd9a09bb7deb87b8
Justin Turney <justin.turney@gmail.com>
2023-02-20 22:02:50 +0000

    GHA: Stop trying to build FFTW

--- cmake/EinsumsManageFFTLibraries.cmake
+++ cmake/EinsumsManageFFTLibraries.cmake
@@ -34,6 +34,7 @@ function(fft_fftw3)
     endif()
 endfunction()
 
+# Unable to figure out how to build single and double precision at once.
 function(build_fftw3)
     # Machinery for running the external project
     set(EXTERNAL_FFTW_VERSION 3.3.10)
@@ -60,22 +61,19 @@ function(build_fftw3)
         build-fftw
         URL ${EINSUMS_BUILD_OWN_FFTW_URL}
         URL_MD5 ${EINSUMS_BUILD_OWN_FFTW_MD5}
-        CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTS=OFF -DENABLE_OPENMP=ON -DENABLE_FLOAT=ON -DDISABLE_FORTRAN=ON ${FFTW_ARCH}
+        CMAKE_ARGS -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER} -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER} -DCMAKE_POSITION_INDEPENDENT_CODE=ON  -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTS=OFF -DENABLE_OPENMP=ON -DENABLE_FLOAT=ON ${FFTW_ARCH}
     )
     ExternalProject_get_property(build-fftw INSTALL_DIR)
-    set(FFTW_LIBRARIES ${INSTALL_DIR}/lib/libfftw3f${CMAKE_STATIC_LIBRARY_SUFFIX})
-    set(FFTW_INCLUDE_DIRS ${INSTALL_DIR}/include)
+    set(FFTW_SINGLE_LIBRARIES ${INSTALL_DIR}/lib/libfftw3f${CMAKE_STATIC_LIBRARY_SUFFIX})
+    set(FFTW_SINGLE_INCLUDE_DIRS ${INSTALL_DIR}/include)
 
-    add_library(fftw STATIC IMPORTED GLOBAL)
+    add_library(fftw STATIC IMPORTED)
     set_target_properties(fftw
         PROPERTIES
             IMPORTED_LOCATION ${FFTW_LIBRARIES}
             INTERFACE_INCLUDE_DIRECTORIES ${FFTW_INCLUDE_DIRS}
     )
-    # target_link_libraries(fftw INTERFACE ${FFTW_LIBRARIES})
-    # target_include_directories(fftw INTERFACE ${FFTW_INCLUDE_DIRS})
     add_dependencies(fftw build-fftw)
-
     add_library(FFT::FFT ALIAS fftw)
 
     set(EINSUMS_FFT_LIBRARY FFTW3 PARENT_SCOPE)
@@ -88,9 +86,9 @@ elseif(EINSUMS_FFT_LIBRARY MATCHES FFTW3)
     # Check for fftw3
     fft_fftw3()
 
-    if (NOT TARGET FFT::FFT)
-        build_fftw3()
-    endif()
+    # if (NOT TARGET FFT::FFT)
+        # build_fftw3()
+    # endif()
 endif()
 
 # Make sure an FFT library was found
