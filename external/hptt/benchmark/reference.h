d7a78b531677e365dec6971d97fac87918c1e45e
Justin Turney <justin.turney@gmail.com>
2023-09-15 14:11:05 -0400

    Use hptt from forked copy.

--- external/hptt/benchmark/reference.h
+++
@@ -1,6 +0,0 @@
-#include <stdlib.h>
-
-template<typename floatType>
-void transpose_ref( uint32_t *size, uint32_t *perm, int dim, 
-      const floatType* __restrict__ A, floatType alpha, 
-      floatType* __restrict__ B, floatType beta, const bool conjA);
