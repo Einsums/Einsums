c47f056b11dde2d2fd743f5dd8ea71cb070d098b
Lori A. Burns <lori.burns@gmail.com>
2023-09-21 23:32:25 -0400

    try 1
    try 2
    
    try 3
    
    try 3
    
    try 5
    
    try 6
    
    try 7
    
    try 8
    
    try 9
    
    try 10
    
    try 11
    
    try 12
    
    try 13
    
    try 12
    
    try 13
    
    try 14
    
    try 15
    
    try 16

--- cmake/Findnetlib.cmake
+++ cmake/Findnetlib.cmake
@@ -3,4 +3,6 @@ include(FindPackageHandleStandardArgs)
 find_package(CBLAS MODULE GLOBAL)
 find_package(LAPACKE MODULE GLOBAL)
 
+set(CBLAS_FOUND 1)
+set(LAPACKE_FOUND 1)
 find_package_handle_standard_args(netlib DEFAULT_MSG CBLAS_FOUND LAPACKE_FOUND)
