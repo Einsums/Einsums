1347fd214585edc239551576cd85a51ab587ad72
Justin Turney <justin.turney@gmail.com>
2023-10-07 05:40:15 -0400

    DOCS: Pull 7-Oct-23 version of exhale from github.

--- docs/conf.py
+++ docs/conf.py
@@ -13,10 +13,9 @@ import datetime
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
+import os
+import sys
+sys.path.insert(0, os.path.abspath('sphinxext'))
 
 # -- Project information -----------------------------------------------------
 
@@ -46,7 +45,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'sphinxext']
 
 
 # -- Options for HTML output -------------------------------------------------
