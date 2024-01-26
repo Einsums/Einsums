84ea44d9a4566f69eb81ceef5e97ccd9366c8fd4
Connor Briggs <cgbriggs99@gmail.com>
2024-01-26 09:49:45 -0500

    Builds and runs!

--- src/include/einsums/GPULinearAlgebra.hpp
+++ src/include/einsums/GPULinearAlgebra.hpp
@@ -38,19 +38,46 @@ __global__ void dot_kernel(CDataType C_prefactor, CDataType *C,
                            std::conditional_t<sizeof(ADataType) < sizeof(BDataType), BDataType, ADataType> AB_prefactor, const ADataType *A,
                            const BDataType *B, size_t elements);
 
-template <typename T>
-void gemm(hipblasOperation_t transa, hipblasOperation_t transb, int m, int n, int k, const T *alpha, const T *a, int lda, const T *b,
-          int ldb, const T *beta, T *c, int ldc);
+EINSUMS_EXPORT void gemm(hipblasOperation_t transa, hipblasOperation_t transb, int m, int n, int k, const float *alpha, const float *a, int lda, const float *b,
+          int ldb, const float *beta, float *c, int ldc);
 
-template <typename T>
-void ger(int m, int n, const T *alpha, const T *x, int incx, const T *y, int incy, T *A, int lda);
+EINSUMS_EXPORT void gemm(hipblasOperation_t transa, hipblasOperation_t transb, int m, int n, int k, const double *alpha, const double *a, int lda, const double *b,
+          int ldb, const double *beta, double *c, int ldc);
 
-template <typename T>
-void gemv(hipblasOperation_t transa, int m, int n, const T *alpha, const T *a, int lda, const T *x, int incx, const T *beta, T *y,
+EINSUMS_EXPORT void gemm(hipblasOperation_t transa, hipblasOperation_t transb, int m, int n, int k, const hipComplex *alpha, const hipComplex *a, int lda, const hipComplex *b,
+          int ldb, const hipComplex *beta, hipComplex *c, int ldc);
+
+EINSUMS_EXPORT void gemm(hipblasOperation_t transa, hipblasOperation_t transb, int m, int n, int k, const hipDoubleComplex *alpha, const hipDoubleComplex *a, int lda, const hipDoubleComplex *b,
+          int ldb, const hipDoubleComplex *beta, hipDoubleComplex *c, int ldc);
+
+
+EINSUMS_EXPORT void ger(int m, int n, const float *alpha, const float *x, int incx, const float *y, int incy, float *A, int lda);
+
+EINSUMS_EXPORT void ger(int m, int n, const double *alpha, const double *x, int incx, const double *y, int incy, double *A, int lda);
+
+EINSUMS_EXPORT void ger(int m, int n, const hipComplex *alpha, const hipComplex *x, int incx, const hipComplex *y, int incy, hipComplex *A, int lda);
+
+EINSUMS_EXPORT void ger(int m, int n, const hipDoubleComplex *alpha, const hipDoubleComplex *x, int incx, const hipDoubleComplex *y, int incy, hipDoubleComplex *A, int lda);
+
+EINSUMS_EXPORT void gemv(hipblasOperation_t transa, int m, int n, const float *alpha, const float *a, int lda, const float *x, int incx, const float *beta, float *y,
           int incy);
 
-template <typename T>
-void scal(int size, const T *alpha, T *x, int incx);
+EINSUMS_EXPORT void gemv(hipblasOperation_t transa, int m, int n, const double *alpha, const double *a, int lda, const double *x, int incx, const double *beta, double *y,
+          int incy);
+
+EINSUMS_EXPORT void gemv(hipblasOperation_t transa, int m, int n, const hipComplex *alpha, const hipComplex *a, int lda, const hipComplex *x, int incx, const hipComplex *beta, hipComplex *y,
+          int incy);
+
+EINSUMS_EXPORT void gemv(hipblasOperation_t transa, int m, int n, const hipDoubleComplex *alpha, const hipDoubleComplex *a, int lda, const hipDoubleComplex *x, int incx, const hipDoubleComplex *beta, hipDoubleComplex *y,
+          int incy);
+
+EINSUMS_EXPORT void scal(int size, const float *alpha, float *x, int incx);
+
+EINSUMS_EXPORT void scal(int size, const double *alpha, double *x, int incx);
+
+EINSUMS_EXPORT void scal(int size, const hipComplex *alpha, hipComplex *x, int incx);
+
+EINSUMS_EXPORT void scal(int size, const hipDoubleComplex *alpha, hipDoubleComplex *x, int incx);
 
 } // namespace detail
 
