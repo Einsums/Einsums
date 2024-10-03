5c109e357f48d38b59dc65d83f129d9dde5eebee
Connor Briggs <cgbriggs99@gmail.com>
2024-10-03 08:17:18 -0400

    Trying to make the python module work.

--- einsums/__init__.py
+++ einsums/__init__.py
@@ -5,12 +5,15 @@ einsums
 This module allows for interaction with the C++ Einsums library.
 """
 
+import sys
+
 try :
-    import core
-except ModuleNotFoundError as e :
-    import sys
-    
-    raise RuntimeError(f"Path is {sys.path} and version is {sys.version}") from e
+    from . import core
+except (ModuleNotFoundError, ImportError) :
+    try :
+        import core
+    except (ModuleNotFoundError, ImportError) as e :
+        raise RuntimeError(f"Path is {sys.path} and version is {sys.version}") from e
 
 from . import utils
 
