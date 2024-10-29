9ae04a8a11acdf49f7a5a8c879f949558cbb2d1d
Connor Briggs <cgbriggs99@gmail.com>
2024-10-29 15:16:02 -0400

    Removed extra zero in test, and put it in the right test.

--- tests/Tensor.cpp
+++ tests/Tensor.cpp
@@ -157,6 +157,30 @@ TEST_CASE("TensorView creation", "[tensor]") {
     for (int i = 0, ij = 0; i < 3; i++)
         for (int j = 0; j < 9; j++, ij++)
             REQUIRE(viewA(i, j) == ij);
+
+    double *array = new double[100];
+
+    for (int i = 0; i < 100; i++) {
+        array[i] = i;
+    }
+
+    {
+
+        TensorView<double, 2>       view1{array, Dim<2>{10, 10}}, view2{array, Dim{10, 10}, Stride{10, 1}};
+        const TensorView<double, 2> const_view1{(const double *)array, Dim<2>{10, 10}},
+            const_view2{(const double *)array, Dim{10, 10}, Stride{10, 1}};
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                REQUIRE(view1(i, j) == array[10 * i + j]);
+                REQUIRE(const_view1(i, j) == array[10 * i + j]);
+                REQUIRE(view2(i, j) == array[10 * i + j]);
+                REQUIRE(const_view2(i, j) == array[10 * i + j]);
+            }
+        }
+    }
+
+    delete[] array;
 }
 
 TEST_CASE("Tensor-2D HDF5") {
@@ -187,29 +211,6 @@ TEST_CASE("Tensor-2D HDF5") {
         REQUIRE(B(3, 5) == 4.0);
         REQUIRE(B(3, 8) == 5.0);
     }
-
-    double *array = new double[100];
-
-    for (int i = 0; i < 100; i++) {
-        array[i] = i;
-    }
-    {
-
-        TensorView<double, 2>       view1{array, Dim<2>{10, 10}}, view2{array, Dim{10, 10}, Stride{10, 1}};
-        const TensorView<double, 2> const_view1{(const double *)array, Dim<2>{10, 10}},
-            const_view2{(const double *)array, Dim{10, 10}, Stride{10, 1}};
-
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 100; j++) {
-                REQUIRE(view1(i, j) == array[10 * i + j]);
-                REQUIRE(const_view1(i, j) == array[10 * i + j]);
-                REQUIRE(view2(i, j) == array[10 * i + j]);
-                REQUIRE(const_view2(i, j) == array[10 * i + j]);
-            }
-        }
-    }
-
-    delete[] array;
 }
 
 TEST_CASE("Tensor-1D HDF5") {
