f7790cd4cb4225956b3ed95db525755dc00ec7b1
Connor Briggs <cgbriggs99@gmail.com>
2024-10-30 14:33:39 -0400

    Added debug prints.

--- einsums/__init__.py
+++ einsums/__init__.py
@@ -5,30 +5,49 @@ einsums
 This module allows for interaction with the C++ Einsums library.
 """
 
+print("Before imports.")
+
 import sys
 import os
 import atexit
+
+print("After system imports. Setting up module path.")
 
 __modpath = os.path.dirname(__file__)
 
 if __modpath not in sys.path :
     sys.path.append(__modpath)
 
+print("Set up the module path. Importing core.")
+
 try :
     from . import core
+    print("Imported core. Checking GPU compatibility.")
     if core.gpu_enabled() :
+        print("Importing gpu_except.")
         from . import gpu_except
+        print("Imported GPU except.")
 except (ModuleNotFoundError, ImportError) :
     try :
+        print("First try failed. Trying again.")
         import core
+        print("Imported core. Checking GPU compatibility.")
         if core.gpu_enabled() :
+            print("Importing gpu_except.")
             import gpu_except
+            print("Imported gpu_except.")
     except (ModuleNotFoundError, ImportError) as e :
         raise RuntimeError(f"File is {__file__}, path is {sys.path} and version is {sys.version}") from e
 
+print("Imported C++ libraries successfully. Importing Python modules.")
+
+print("Importing utils.")
 from . import utils # pylint: disable=wrong-import-position
+print("Imported utils. Initializing.")
 
 core.initialize()
+
+print('Initialized')
 
 __outfile = False
 
@@ -40,4 +59,6 @@ it is a file name, that will be used as the timing file, and the timing will be 
     """
     __outfile = out_arg
 
+print("Registering finalization.")
 atexit.register(core.finalize, __outfile)
+print("Registered finalization. Everything finished.")
