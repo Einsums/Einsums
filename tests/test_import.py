ad5aaa7812130b82d92b26645a62a1971bf7b1f1
Connor Briggs <cgbriggs99@gmail.com>
2024-10-31 14:16:46 -0400

    Add more flushes to prints.

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,9 +1,11 @@
 import pytest # pylint: disable=unused-import
 
 def test_import() :
+    print("Importing einsums.", flush = True)
     import einsums as ein # pylint: disable=import-outside-toplevel,unused-import
+    print("Imported einsums.", flush=True)
 
 if __name__ == "__main__" :
-    print("Importing einsums.")
-    import einsums as ein
-    print("Imported einsums.")
+    print("Importing einsums.", flush = True)
+    import einsums as ein # pylint: disable=import-outside-toplevel,unused-import
+    print("Imported einsums.", flush=True)
