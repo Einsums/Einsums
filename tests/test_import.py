d810f82a3741635d6921f1f973faf0623dea98d1
Connor Briggs <cgbriggs99@gmail.com>
2024-10-31 10:11:38 -0400

    Test import separately.

---
+++ tests/test_import.py
@@ -0,0 +1,7 @@
+import pytest # pylint: disable=unused-import
+
+def test_import() :
+    import einsums as ein # pylint: disable=import-outside-toplevel,unused-import
+
+if __name__ == "__main__" :
+    print("Imported einsums.")
