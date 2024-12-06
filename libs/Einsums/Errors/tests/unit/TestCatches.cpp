7a28edcdcec8a3842b4f524493210bf2c04d06b8
Connor Briggs <cgbriggs99@gmail.com>
2024-12-06 12:58:34 -0500

    More basic test.

--- libs/Einsums/Errors/tests/unit/TestCatches.cpp
+++ libs/Einsums/Errors/tests/unit/TestCatches.cpp
@@ -8,7 +8,17 @@ static void thrower() {
     EINSUMS_THROW_EXCEPTION(einsums::dimension_error, "Test error.");
 }
 
+static void thrower2() {
+    throw einsums::dimension_error("Test error2.");
+}
+
 TEST_CASE("Test catching", "[error]") {
+    try {
+        thrower2();
+    } catch(const einsums::dimension_error &e) {
+        REQUIRE(true);
+    }
+
     try {
         thrower();
     } catch(einsums::dimension_error &e) {
