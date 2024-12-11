2a62ca3d5e13d9624f3f35fdc480d497bd2da1d3
Connor Briggs <cgbriggs99@gmail.com>
2024-12-11 15:45:16 -0500

    Fixed configuration bugs.

--- libs/create_module_skeleton/__main__.py
+++ libs/create_module_skeleton/__main__.py
@@ -10,16 +10,19 @@ def main() :
 
     parser.add_argument("library", help = "The name of the top-level library.")
     parser.add_argument("module", help = "The name of the module within the top-level library.")
+    parser.add_argument("--python", help = "Set this flag if the module is a Python extension module.", action= "store_true")
+    parser.add_argument("--gpu", help = "Set this flag if the module is a GPU module.", action = "store_true")
 
     args = parser.parse_args()
 
     module = args.module
     library = args.library
+    lib_symb = library if not args.python else "${EINSUMS_PYTHON_LIB_NAME}"
 
-    build_structure.build_structure(os.curdir, library, module, **vars(args))
+    build_structure.build_structure(os.curdir, library, module, lib_symb = lib_symb, **vars(args))
 
-    configure_toplevel.configure_cmake(os.curdir, library, **vars(args))
-    configure_toplevel.configure_module_docs(os.curdir, library, **vars(args))
+    configure_toplevel.configure_cmake(os.curdir, library, lib_symb = lib_symb, **vars(args))
+    configure_toplevel.configure_module_docs(os.curdir, library, lib_symb = lib_symb, **vars(args))
 
 if __name__ == "__main__" :
     main()
--- libs/create_module_skeleton/configure_toplevel.py
+++ libs/create_module_skeleton/configure_toplevel.py
@@ -16,16 +16,27 @@ def configure_cmake(output_base, lib_name, **kwargs):
         with open(os.path.join(output_base, lib_name, "preamble.txt"), "r") as fp:
             preamble = fp.read()
 
-    modules = "\n".join(
-        os.listdir(os.path.join(output_base, lib_name)),
+    modules = "\n  ".join(
+        filter(
+            lambda x: os.path.isdir(os.path.join(output_base, lib_name, x)),
+            os.listdir(os.path.join(output_base, lib_name)),
+        )
     )
 
+    modules = modules.rstrip()
+
     with open(os.path.join(output_base, lib_name, "CMakeLists.txt"), "w+") as fp:
+        if kwargs["gpu"] :
+            print("if(EINSUMS_WITH_GPU_SUPPORT)", file = fp)
+
         fp.write(
             format.format(
                 modules=modules, preamble=preamble, lib_name=lib_name, **kwargs
             )
         )
+
+        if kwargs["gpu"] :
+            print("endif()")
 
 
 def configure_module_docs(output_base, lib_name, **kwargs):
@@ -39,7 +50,10 @@ def configure_module_docs(output_base, lib_name, **kwargs):
     module_docs = "\n    ".join(
         map(
             lambda x: os.path.join("/libs", lib_name, x, "docs", "index.rst"),
-            os.listdir(os.path.join(output_base, lib_name)),
+            filter(
+                lambda x: os.path.isdir(os.path.join(output_base, lib_name, x)),
+                os.listdir(os.path.join(output_base, lib_name)),
+            ),
         )
     )
 
