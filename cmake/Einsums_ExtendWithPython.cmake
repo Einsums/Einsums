fc81654fec646d17067c43d955d23b8810a97f1a
Justin Turney <justin.turney@gmail.com>
2024-12-15 12:32:16 -0500

    Python: Fixed for linux potentially.

--- cmake/Einsums_ExtendWithPython.cmake
+++ cmake/Einsums_ExtendWithPython.cmake
@@ -1,20 +1,17 @@
 function(einsums_extend_with_python target)
-  if (APPLE)
-    target_link_options(${target} PUBLIC -undefined dynamic_lookup)
-  endif ()
+	#  if (APPLE)
+	#   target_link_options(${target} PUBLIC -undefined dynamic_lookup)
+	#  endif ()
 
-  target_link_libraries(${target} PRIVATE Python3::Module)
-  target_link_libraries(${target} PRIVATE pybind11::module pybind11::lto pybind11::windows_extras)
+	#target_include_directories(${target} PRIVATE ${Python_INCLUDE_DIRS})
+  target_link_libraries(${target} PRIVATE pybind11::module pybind11::lto pybind11::windows_extras Python3::Python)
 
-  if (MSVC)
-    target_link_libraries(${target} PRIVATE pybind11::windows_extras)
-  endif ()
+  pybind11_extension(${target})
 
   if (NOT MSVC AND NOT ${CMAKE_BUILD_TYPE} MATCHES Debug|RelWithDebInfo)
     # Strip unnecessary sections of the binary on Linux/macOS
     pybind11_strip(${target})
   endif ()
-  pybind11_extension(${target})
 
   set_target_properties(${target} PROPERTIES PREFIX "" DEBUG_POSTFIX "")
 endfunction()
