184e5e6a6ff84d0693ea870bca8db3e8ecf2f932
Justin Turney <justin.turney@gmail.com>
2024-12-15 09:48:16 -0500

    Python: Fixes Python module build on my Mac.
    Modeled after Psi4.

--- cmake/Einsums_ExtendWithPython.cmake
+++ cmake/Einsums_ExtendWithPython.cmake
@@ -1,26 +1,20 @@
-function(einsums_extend_with_python target modtype)
-    if(APPLE)
-        target_link_options(${target} PUBLIC -undefined dynamic_lookup)
-    endif()
+function(einsums_extend_with_python target)
+  if (APPLE)
+    target_link_options(${target} PUBLIC -undefined dynamic_lookup)
+  endif ()
 
-    target_link_libraries(${target} PRIVATE pybind11::lto)
+  target_link_libraries(${target} PRIVATE Python3::Module)
+  target_link_libraries(${target} PRIVATE pybind11::module pybind11::lto pybind11::windows_extras)
 
-    if(modtype STREQUAL "SHARED")
-        target_link_libraries(${target} PRIVATE pybind11::embed)
-    elseif(modtype STREQUAL "MODULE")
-        target_link_libraries(${target} PRIVATE pybind11::module ${Python3_LIBRARIES})
-    endif()
+  if (MSVC)
+    target_link_libraries(${target} PRIVATE pybind11::windows_extras)
+  endif ()
 
-    if(MSVC)
-        target_link_libraries(${target} PRIVATE pybind11::windows_extras)
-    endif()
+  if (NOT MSVC AND NOT ${CMAKE_BUILD_TYPE} MATCHES Debug|RelWithDebInfo)
+    # Strip unnecessary sections of the binary on Linux/macOS
+    pybind11_strip(${target})
+  endif ()
+  pybind11_extension(${target})
 
-    if(NOT MSVC AND NOT ${CMAKE_BUILD_TYPE} MATCHES Debug|RelWithDebInfo)
-        # Strip unnecessary sections of the binary on Linux/macOS
-        pybind11_strip(${target})
-    endif()
-
-    pybind11_extension(${target})
-
-    set_target_properties(${target} PROPERTIES PREFIX "" DEBUG_POSTFIX "")
+  set_target_properties(${target} PROPERTIES PREFIX "" DEBUG_POSTFIX "")
 endfunction()
