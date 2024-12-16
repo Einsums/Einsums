be46e4cdb57a2cb004a0c52abaaff6ed9401fa41
Connor Briggs <cgbriggs99@gmail.com>
2024-12-16 15:52:18 -0500

    Config map is visible to Python.

--- libs/EinsumsPy/Core/src/Export.cpp
+++ libs/EinsumsPy/Core/src/Export.cpp
@@ -1,5 +1,6 @@
 #include <Einsums/Config.hpp>
 
+#include <Einsums/Config/Types.hpp>
 #include <Einsums/Errors/Error.hpp>
 #include <Einsums/Runtime.hpp>
 
@@ -7,6 +8,7 @@
 #include <pybind11/pybind11.h>
 
 namespace py = pybind11;
+using namespace einsums;
 
 bool gpu_enabled() {
 #ifdef EINSUMS_COMPUTE_CODE
@@ -19,6 +21,25 @@ bool gpu_enabled() {
 void export_Core(py::module_ &mod) {
     mod.def("gpu_enabled", gpu_enabled)
         .def("initialize", einsums::initialize)
-        .def("finalize", [](std::string file_name) { einsums::finalize(file_name); }, py::arg("file_name"))
+        .def(
+            "finalize", [](std::string file_name) { einsums::finalize(file_name); }, py::arg("file_name"))
         .def("finalize", [](bool timer_report) { einsums::finalize(timer_report); }, py::arg("timer_report") = false);
+
+    auto config_map = py::class_<einsums::GlobalConfigMap, std::shared_ptr<einsums::GlobalConfigMap>>(mod, "GlobalConfigMap");
+
+    config_map.def_static("get_singleton", einsums::GlobalConfigMap::get_singleton)
+        .def("empty", [](GlobalConfigMap &self) { return self.empty(); })
+        .def("size", [](GlobalConfigMap &self) { return self.size(); })
+        .def("max_size", [](GlobalConfigMap &self) { return self.max_size(); })
+        .def("clear", [](GlobalConfigMap &self) { self.clear(); })
+        .def("erase", [](GlobalConfigMap &self, std::string const &str) { return self.erase(str); })
+        .def("at_string", [](GlobalConfigMap &self, std::string const &str) { return self.at_string(str); })
+        .def("at_int", [](GlobalConfigMap &self, std::string const &str) { return self.at_int(str); })
+        .def("at_double", [](GlobalConfigMap &self, std::string const &str) { return self.at_double(str); })
+        .def("get_string", [](GlobalConfigMap &self, std::string const &str) { return self.get_string(str); })
+        .def("get_int", [](GlobalConfigMap &self, std::string const &str) { return self.get_int(str); })
+        .def("get_double", [](GlobalConfigMap &self, std::string const &str) { return self.get_double(str); })
+        .def("set_string", [](GlobalConfigMap &self, std::string const &str, std::string &val) { self.get_string(str) = val; })
+        .def("set_int", [](GlobalConfigMap &self, std::string const &str, std::int64_t val) { self.get_int(str) = val; })
+        .def("set_double", [](GlobalConfigMap &self, std::string const &str, double val) { self.get_double(str) = val; });
 }
