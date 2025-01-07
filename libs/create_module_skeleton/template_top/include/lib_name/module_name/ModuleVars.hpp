77f4e5d542bdef35d54a1298bfd6aaa5ad7df43e
Connor Briggs <cgbriggs99@gmail.com>
2025-01-07 14:29:04 -0500

    Added locking to the module vars template.

--- libs/create_module_skeleton/template_top/include/lib_name/module_name/ModuleVars.hpp
+++ libs/create_module_skeleton/template_top/include/lib_name/module_name/ModuleVars.hpp
@@ -8,14 +8,28 @@ namespace detail {{
 
 // TODO: This class can be freely changed. It is provided as a starting point for your convenience.
 
-class EINSUMS_EXPORT {lib_name}_{module_name}_vars {{
+class EINSUMS_EXPORT {lib_name}_{module_name}_vars final {{
     EINSUMS_SINGLETON_DEF({lib_name}_{module_name}_vars)
 
 public:
     // Put module-global variables here.
 
+    inline void lock() const {
+        lock_.lock();
+    }
+
+    inline bool try_lock() const {
+        return lock_.try_lock();
+    }
+
+    inline void unlock() const {
+        lock_.unlock();
+    }
+
 private:
     explicit {lib_name}_{module_name}_vars() = default;
+
+    mutable std::recursive_mutex lock_;
 
 }};
 
