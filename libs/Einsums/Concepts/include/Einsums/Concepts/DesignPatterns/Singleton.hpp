12bea4a02b250b42e7ca890608f66d8897357e0b
Connor Briggs <cgbriggs99@gmail.com>
2025-01-07 14:07:05 -0500

    Fix up the singleton.

--- libs/Einsums/Concepts/include/Einsums/Concepts/DesignPatterns/Singleton.hpp
+++ libs/Einsums/Concepts/include/Einsums/Concepts/DesignPatterns/Singleton.hpp
@@ -5,7 +5,7 @@
  *
  * Turns a C++ class into a singleton. Place this at the beginning of the class. You will then need to define a private
  * constructor with no arguments to actually construct the singleton stuff. This macro is only for the definition of the
- * class and does not contain any code. Make sure to use a matching \c EINSUMS_SINGLETON_IMPL somewhere else to 
+ * class and does not contain any code. Make sure to use a matching \c EINSUMS_SINGLETON_IMPL somewhere else to
  * get the code to compile.
  *
  * This will provide the two methods <tt>Type &get_singleton()</tt> and <tt>finalize_singleton()</tt>.
@@ -15,14 +15,11 @@
 #define EINSUMS_SINGLETON_DEF(Type)                                                                                                        \
   private:                                                                                                                                 \
     class PrivateConstructorStuff {};                                                                                                      \
-    static Type **get_singleton_pointer();                                                                                                 \
                                                                                                                                            \
   public:                                                                                                                                  \
     Type(PrivateConstructorStuff ignore) : Type() {                                                                                        \
     }                                                                                                                                      \
-    static Type &get_singleton();                                                                                                          \
-                                                                                                                                           \
-    static void finalize_singleton();
+    static Type &get_singleton();
 
 /**
  * @def EINSUMS_SINGLETON_IMPL
@@ -30,24 +27,7 @@
  * Creates the code for managing a singleton.
  */
 #define EINSUMS_SINGLETON_IMPL(Type)                                                                                                       \
-    Type **Type::get_singleton_pointer() {                                                                                                 \
-        static Type *singleton_instance = nullptr;                                                                                         \
-        return &singleton_instance;                                                                                                        \
-    }                                                                                                                                      \
-                                                                                                                                           \
     Type &Type::get_singleton() {                                                                                                          \
-        Type **singleton_instance = get_singleton_pointer();                                                                               \
-                                                                                                                                           \
-        if (*singleton_instance == nullptr) {                                                                                              \
-            *singleton_instance = Type(PrivateConstructorStuff());                                                                         \
-        }                                                                                                                                  \
-        return **singleton_instance;                                                                                                       \
-    }                                                                                                                                      \
-                                                                                                                                           \
-    void Type::finalize_singleton() {                                                                                                      \
-        Type **singleton_instance = get_singleton_pointer();                                                                               \
-                                                                                                                                           \
-        if (*singleton_instance != nullptr) {                                                                                              \
-            delete *singleton_instance;                                                                                                    \
-        }                                                                                                                                  \
+        static std::unique_ptr<Type> singleton_instance = std::make_unique<Type>(PrivateConstructorStuff());                               \
+        return *singleton_instance;                                                                                                        \
     }
