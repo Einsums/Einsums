130ae0ed27b4004f9b6764ce068a5ea1ac99d1bc
Connor Briggs <cgbriggs99@gmail.com>
2025-01-07 14:55:47 -0500

    Fix utils.

--- libs/Einsums/Utilities/src/InitModule.cpp
+++ libs/Einsums/Utilities/src/InitModule.cpp
@@ -2,6 +2,8 @@
 #include <Einsums/Utilities/Random.hpp>
 #include <Einsums/Runtime.hpp>
 #include <random>
+#include "Einsums/Runtime/ShutdownFunction.hpp"
+#include "Einsums/Runtime/StartupFunction.hpp"
 
 /*
  * Set up the internal state of the module. If the module does not need to be set up, then this
@@ -13,8 +15,8 @@
 einsums::init_Einsums_Utilities::init_Einsums_Utilities() {
     // Auto-generated code. Do not touch if you are unsure of what you are doing.
     // Instead, modify the other functions below.
-    einsums::detail::global_startup_functions.push_back(einsums::initialize_Einsums_Utilities);
-    einsums::detail::global_shutdown_functions.push_back(einsums::initialize_Einsums_Utilities);
+    einsums::register_pre_startup_function(einsums::initialize_Einsums_Utilities);
+    einsums::register_shutdown_function(einsums::finalize_Einsums_Utilities);
 }
 
 einsums::init_Einsums_Utilities einsums::detail::initialize_module_Einsums_Utilities;
