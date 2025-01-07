5b5e266de6dbf5608d9ce2d4454a078d365fb60b
Justin Turney <justin.turney@gmail.com>
2025-01-07 09:51:44 -0500

    Removed some commented out code.

--- libs/Einsums/Errors/src/ErrorNames.cpp
+++ libs/Einsums/Errors/src/ErrorNames.cpp
@@ -1,5 +1,5 @@
 //--------------------------------------------------------------------------------------------
-// Copyright (c) The Einsums Developers. All rights reserved.
+// Copyright (c) The Einsums Developers. All Rights Reserved.
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 //--------------------------------------------------------------------------------------------
 
@@ -10,10 +10,6 @@
 namespace einsums::detail {
 
 std::string make_error_message(std::string_view const &type_name, char const *str, std::source_location const &location) {
-    // if (!__is_library_initialized) {
-    // return fmt::format("{}:{}:{}:\nIn {}\n{}: {}\nLibrary is not initialized. Try initializing first to see if the error goes away.",
-    // location.file_name(), location.line(), location.column(), location.function_name(), type_name, str);
-    // }
     return fmt::format("{}:{}:{}:\nIn {}\n{}: {}", location.file_name(), location.line(), location.column(), location.function_name(),
                        type_name, str);
 }
