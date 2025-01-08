d6cdf94532b7fd8a596e0d4773bd89b81ee2acb9
Justin Turney <justin.turney@gmail.com>
2025-01-08 14:09:32 -0500

    Delete old src directory

--- src/TensorAlgebra.cpp
+++
@@ -1,9 +0,0 @@
-//----------------------------------------------------------------------------------------------
-// Copyright (c) The Einsums Developers. All rights reserved.
-// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
-//----------------------------------------------------------------------------------------------
-
-#include "einsums/TensorAlgebra.hpp"
-
-BEGIN_EINSUMS_NAMESPACE_CPP(einsums::tensor_algebra)
-END_EINSUMS_NAMESPACE_CPP(einsums::tensor_algebra)
--- src/polynomial/Laguerre.cpp
+++
@@ -1,9 +0,0 @@
-//----------------------------------------------------------------------------------------------
-// Copyright (c) The Einsums Developers. All rights reserved.
-// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
-//----------------------------------------------------------------------------------------------
-
-#include "einsums/polynomial/Laguerre.hpp"
-
-BEGIN_EINSUMS_NAMESPACE_CPP(einsums::polynomial::laguerre)
-END_EINSUMS_NAMESPACE_CPP(einsums::polynomial::laguerre)
