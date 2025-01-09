eda1c49975c9fb254911aa2859f09d1d208e20a1
Justin Turney <justin.turney@gmail.com>
2025-01-09 09:21:22 -0500

    Somehow forgot an old file.

--- src/LinearAlgebra.cpp
+++
@@ -1,9 +0,0 @@
-//----------------------------------------------------------------------------------------------
-// Copyright (c) The Einsums Developers. All rights reserved.
-// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
-//----------------------------------------------------------------------------------------------
-
-#include "einsums/_Common.hpp"
-
-BEGIN_EINSUMS_NAMESPACE_CPP(einsums::linear_algebra)
-END_EINSUMS_NAMESPACE_CPP(einsums::linear_algebra)
