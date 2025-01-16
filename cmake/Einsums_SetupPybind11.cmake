f98155f99caa86fc7c83b4a709f4063d52b3bc8b
Justin Turney <justin.turney@gmail.com>
2025-01-16 15:10:37 -0500

    CMake: FetchContent with Pybind11 does seem to work. Require that the user provide it.

--- cmake/Einsums_SetupPybind11.cmake
+++ cmake/Einsums_SetupPybind11.cmake
@@ -3,14 +3,6 @@
 # Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 #----------------------------------------------------------------------------------------------
 
-include(FetchContent)
-
-include(FetchContent)
-fetchcontent_declare(
-  pybind11
-  GIT_REPOSITORY https://github.com/pybind/pybind11
-  GIT_TAG v2.13.6
-  FIND_PACKAGE_ARGS 2.13
-)
-
-fetchcontent_makeavailable(pybind11)
+if(EINSUMS_BUILD_PYTHON)
+  find_package(pybind11 2.11 REQUIRED)
+endif()
