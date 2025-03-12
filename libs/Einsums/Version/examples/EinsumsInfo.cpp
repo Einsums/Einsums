9df90af5793dfc885a17223c87cdcb77a0c52f4e
Justin Turney <justin.turney@gmail.com>
2025-03-12 09:20:12 -0400

    Adds Version submodule (#133)
    * Version: Add Version submodule.
    
    * Version: Report version information to the info log on startup.

---
+++ libs/Einsums/Version/examples/EinsumsInfo.cpp
@@ -0,0 +1,15 @@
+//--------------------------------------------------------------------------------------------
+// Copyright (c) The Einsums Developers. All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+//--------------------------------------------------------------------------------------------
+
+#include <Einsums/Print.hpp>
+#include <Einsums/Version.hpp>
+
+int main() {
+    using namespace einsums;
+
+    println(complete_version());
+
+    return EXIT_SUCCESS;
+}
