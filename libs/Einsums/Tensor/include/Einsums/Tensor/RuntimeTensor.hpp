1d7f38793a5142069f217b2e1c4d6e69bc44bc85
Connor Briggs <cgbriggs99@gmail.com>
2025-03-27 10:59:48 -0400

    Trying this fix.

--- libs/Einsums/Tensor/include/Einsums/Tensor/RuntimeTensor.hpp
+++ libs/Einsums/Tensor/include/Einsums/Tensor/RuntimeTensor.hpp
@@ -708,8 +708,9 @@ struct EINSUMS_EXPORT RuntimeTensor : public tensor_base::CoreTensor,
             }                                                                                                                              \
             T            *this_data = this->data();                                                                                        \
             const TOther *b_data    = b.data();                                                                                            \
+            size_t        elements  = size();                                                                                              \
             EINSUMS_OMP_PARALLEL_FOR_SIMD                                                                                                  \
-            for (size_t sentinel = 0; sentinel < size(); sentinel++) {                                                                     \
+            for (size_t sentinel = 0; sentinel < elements; sentinel++) {                                                                   \
                 if constexpr (IsComplexV<T> && !IsComplexV<TOther> && !std::is_same_v<RemoveComplexT<T>, TOther>) {                        \
                     this->_data[sentinel] OP(T)(RemoveComplexT<T>) b_data[sentinel];                                                       \
                 } else if constexpr (!IsComplexV<T> && IsComplexV<TOther>) {                                                               \
@@ -731,8 +732,9 @@ struct EINSUMS_EXPORT RuntimeTensor : public tensor_base::CoreTensor,
                 EINSUMS_THROW_EXCEPTION(dimension_error,                                                                                   \
                                         "Can not perform the operation with runtime tensor and view of different dimensions!");            \
             }                                                                                                                              \
+            size_t elements = size();                                                                                                      \
             EINSUMS_OMP_PARALLEL_FOR_SIMD                                                                                                  \
-            for (size_t sentinel = 0; sentinel < size(); sentinel++) {                                                                     \
+            for (size_t sentinel = 0; sentinel < elements; sentinel++) {                                                                   \
                 thread_local std::vector<size_t> index(rank());                                                                            \
                 sentinel_to_indices(sentinel, this->_strides, index);                                                                      \
                 if constexpr (IsComplexV<T> && !IsComplexV<TOther> && !std::is_same_v<RemoveComplexT<T>, TOther>) {                        \
