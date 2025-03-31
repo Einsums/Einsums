5f483d39f8d4e51e7bd71dfeff78f6e7d27be4d8
Connor Briggs <cgbriggs99@gmail.com>
2025-03-31 10:40:25 -0400

    Doing one case at a time.

--- libs/Einsums/Tensor/include/Einsums/Tensor/RuntimeTensor.hpp
+++ libs/Einsums/Tensor/include/Einsums/Tensor/RuntimeTensor.hpp
@@ -695,7 +695,7 @@ struct EINSUMS_EXPORT RuntimeTensor : public tensor_base::CoreTensor,
                     this_data[i] OP(T) b.real();                                                                                           \
                 }                                                                                                                          \
             } else {                                                                                                                       \
-                EINSUMS_OMP_PARALLEL_FOR_SIMD                                                                                              \
+                EINSUMS_OMP_PARALLEL_FOR                                                                                                   \
                 for (size_t i = 0; i < elements; i++) {                                                                                    \
                     if constexpr (IsComplexV<T> && !IsComplexV<TOther> && !std::is_same_v<RemoveComplexT<T>, TOther>) {                    \
                         this_data[i] OP(T)(RemoveComplexT<T>) b;                                                                           \
@@ -714,19 +714,20 @@ struct EINSUMS_EXPORT RuntimeTensor : public tensor_base::CoreTensor,
             T            *this_data = this->data();                                                                                        \
             const TOther *b_data    = b.data();                                                                                            \
             size_t        elements  = size();                                                                                              \
-            if constexpr (!IsComplexV<T> && IsComplexV<TOther>) {                                                                          \
+            if constexpr (IsComplexV<T> && !IsComplexV<TOther> && !std::is_same_v<RemoveComplexT<T>, TOther>) {                            \
+                EINSUMS_OMP_PARALLEL_FOR_SIMD                                                                                              \
+                for (size_t sentinel = 0; sentinel < elements; sentinel++) {                                                               \
+                    this_data[sentinel] OP(T)(RemoveComplexT<T>) b_data[sentinel];                                                         \
+                }                                                                                                                          \
+            } else if constexpr (!IsComplexV<T> && IsComplexV<TOther>) {                                                                   \
                 EINSUMS_OMP_PARALLEL_FOR                                                                                                   \
                 for (size_t sentinel = 0; sentinel < elements; sentinel++) {                                                               \
                     this_data[sentinel] OP(T) b_data[sentinel].real();                                                                     \
                 }                                                                                                                          \
             } else {                                                                                                                       \
-                EINSUMS_OMP_PARALLEL_FOR_SIMD                                                                                              \
+                EINSUMS_OMP_PARALLEL_FOR                                                                                                   \
                 for (size_t sentinel = 0; sentinel < elements; sentinel++) {                                                               \
-                    if constexpr (IsComplexV<T> && !IsComplexV<TOther> && !std::is_same_v<RemoveComplexT<T>, TOther>) {                    \
-                        this_data[sentinel] OP(T)(RemoveComplexT<T>) b_data[sentinel];                                                     \
-                    } else {                                                                                                               \
-                        this_data[sentinel] OP(T) b_data[sentinel];                                                                        \
-                    }                                                                                                                      \
+                    this_data[sentinel] OP(T) b_data[sentinel];                                                                            \
                 }                                                                                                                          \
             }                                                                                                                              \
                                                                                                                                            \
