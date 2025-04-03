0d583f0c8d46378c40edb4b2e372f95045beac95
Nathan Harmer <njh80@cam.ac.uk>
2025-04-03 12:11:59 +0100

    FIX: Moved function into bracket.

--- libs/Einsums/TensorAlgebra/include/Einsums/TensorAlgebra/Permute.hpp
+++ libs/Einsums/TensorAlgebra/include/Einsums/TensorAlgebra/Permute.hpp
@@ -167,14 +167,14 @@ void permute(U const UC_prefactor, std::tuple<CIndices...> const &C_indices, CTy
 
         detail::permute(perms.data(), ARank, A_prefactor, A.full_data(), size.data(), offsetA.data(), outerSizeA.data(), 
                         innerStrideA, C_prefactor, C->full_data(), offsetC.data(), outerSizeC.data(), innerStrideC);
-    } else
+    } else 
 #endif    
-        // If the prefactor is zero, set the tensor to zero. This avoids NaNs.
-        if (C_prefactor == T{0.0}) {
-            *C = T{0.0};
-        }
         if constexpr (std::is_same_v<decltype(A_indices), decltype(C_indices)>) {
-        linear_algebra::axpby(A_prefactor, A, C_prefactor, C);
+            // If the prefactor is zero, set the tensor to zero. This avoids NaNs.
+            if (C_prefactor == T{0.0}) {
+                *C = T{0.0};
+            }
+            linear_algebra::axpby(A_prefactor, A, C_prefactor, C);
     } else {    
         // If the prefactor is zero, set the tensor to zero. This avoids NaNs.
         if (C_prefactor == T{0.0}) {
