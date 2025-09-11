faefb0c49f725d2c98c1fbba4cc3902ce15568b1
Connor Briggs <cgbriggs99@gmail.com>
2025-09-11 12:37:19 -0400

    Remove erroneous copyright.

--- libs/Einsums/HPTT/include/Einsums/HPTT/Plan.hpp
+++ libs/Einsums/HPTT/include/Einsums/HPTT/Plan.hpp
@@ -1,8 +1,3 @@
-//----------------------------------------------------------------------------------------------
-// Copyright (c) The Einsums Developers. All rights reserved.
-// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
-//----------------------------------------------------------------------------------------------
-
 /*
   Copyright 2018 Paul Springer
 
